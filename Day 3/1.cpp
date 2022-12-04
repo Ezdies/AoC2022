d4f5cb44f984ea755f08b367989b1f85c2bc2606
Ezdies <303214@office.umcs.pl>
2022-12-04 23:24:23 +0100

    Day 3 Part 1 (first half)

---
+++ Day 3/1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+
+void getInput(std::vector<char> &sharedItems) {
+    std::ifstream ist(R"(F:\CLionProjects\AoC2022\Day 3\data.txt)");
+
+    std::set<char> firstHalf;
+    std::set<char> secondHalf;
+
+    std::string line;
+
+    while (std::getline(ist, line)) {
+        std::string firstHalfLine = line.substr(0, line.size() / 2);
+        std::string secondHalfLine = line.substr(line.size() / 2);
+
+        for (const char c: firstHalfLine) {
+            firstHalf.insert(c);
+        }
+
+        for (const char c: secondHalfLine) {
+            secondHalf.insert(c);
+        }
+
+        std::set_intersection(firstHalf.begin(), firstHalf.end(), secondHalf.begin(), secondHalf.end(), std::back_inserter(sharedItems));
+        firstHalf.clear();
+        secondHalf.clear();
+    }
+}
+
+void printInput(const std::vector<char> &sharedItems) {
+    for (const char ch: sharedItems) {
+        std::cout << ch;
+    }
+}
+
+int main() {
+    std::vector<char> sharedItems;
+    getInput(sharedItems);
+    printInput(sharedItems);
+}
