e35355ec50caccbabed22cc6841c80854a02d744
Ezdies <303214@office.umcs.pl>
2022-12-05 21:14:27 +0100

    Day 5 Part 1 (finished, but not final)

--- Day 5/1.cpp
+++ Day 5/1.cpp
@@ -24,11 +24,19 @@ void getInput(std::vector<Order> &vectorOfOrders) {
 }
 
 void initializeStacks(std::vector<std::stack<char>> &vectorOfStacks) {
-    std::vector<char> vector1{'Z', 'N'};
-    std::vector<char> vector2{'M', 'C', 'D'};
-    std::vector<char> vector3{'P'};
+    std::vector<char> vector1{'G', 'T', 'R', 'W'};
+    std::vector<char> vector2{'G', 'C', 'H', 'P', 'M', 'S', 'V', 'W'};
+    std::vector<char> vector3{'C', 'L', 'T', 'S', 'G', 'M'};
+    std::vector<char> vector4{'J', 'H', 'D', 'M', 'W', 'R', 'F'};
+    std::vector<char> vector5{'P', 'Q', 'L', 'H', 'S', 'W', 'F', 'J'};
+    std::vector<char> vector6{'P', 'J', 'D', 'N', 'F', 'M', 'S'};
+    std::vector<char> vector7{'Z', 'B', 'D', 'F', 'G', 'C', 'S', 'J'};
+    std::vector<char> vector8{'R', 'T', 'B'};
+    std::vector<char> vector9{'H', 'N', 'W', 'L', 'C'};
 
-    std::vector<std::vector<char>> vectorOfVectors{vector1, vector2, vector3};
+
+    std::vector<std::vector<char>> vectorOfVectors{vector1, vector2, vector3, vector4, vector5, vector6, vector7,
+                                                   vector8, vector9};
 
     for (int i = 0; i < vectorOfStacks.size(); i++) {
         for (const char c: vectorOfVectors[i]) {
@@ -37,24 +45,27 @@ void initializeStacks(std::vector<std::stack<char>> &vectorOfStacks) {
     }
 }
 
+void takeQuantityFromStack(const int quantity, std::stack<char> &startingStack, std::vector<char> &takenElements) {
+    for (int i = 0; i < quantity; i++) {
+        takenElements.push_back(startingStack.top());
+        startingStack.pop();
+    }
+}
 
+void putQuantityOnStack(std::stack<char> &destinationStack, std::vector<char> &takenElements) {
+    for (const char c: takenElements) {
+        destinationStack.push(c);
+    }
+    takenElements.clear();
+}
 
-void moveStacks(std::vector<std::stack<char>> &vectorOfStacks, const std::vector<Order>& vectorOfOrders){
-//    std::vector<char> tmpStack;
-//
-//    for(int i = 0; i < vectorOfOrders.size(); i++){
-//        for(int j = 0; j < vectorOfStacks[vectorOfOrders[i].startingStack - 1].size(); j++){
-//            for(int k = 0; k < vectorOfOrders[i].quantity; k++){
-//                tmpStack.push_back(vectorOfStacks[vectorOfOrders[i].startingStack - 1].top());
-//            }
-//        }
-//        vectorOfStacks[vectorOfOrders[i].startingStack - 1].pop();
-//
-//    }
-//    for(const auto c : tmpStack){
-//        std::cout << c << std::endl;
-//    }
-//    std::cout << vectorOfStacks[vectorOfOrders[0].startingStack - 1].top();
+void moveStacks(std::vector<std::stack<char>> &vectorOfStacks, const std::vector<Order> &vectorOfOrders,
+                std::vector<char> &tmpElements) {
+    for (const auto order: vectorOfOrders) {
+        takeQuantityFromStack(order.quantity, vectorOfStacks[order.startingStack - 1],
+                              tmpElements);
+        putQuantityOnStack(vectorOfStacks[order.destinationStack - 1], tmpElements);
+    }
 }
 
 void printStacks(const std::vector<std::stack<char>> &vectorOfStacks) {
@@ -68,6 +79,12 @@ void printStacks(const std::vector<std::stack<char>> &vectorOfStacks) {
     std::cout << std::endl;
 }
 
+void printStackTops(const std::vector<std::stack<char>> &vectorOfStacks){
+    for(const auto &s : vectorOfStacks){
+        std::cout << s.top();
+    }
+}
+
 void printOrders(const std::vector<Order> &vectorOfOrders) {
     for (const auto &ord: vectorOfOrders) {
         std::cout << ord.quantity << " " << ord.startingStack << " " << ord.destinationStack << std::endl;
@@ -75,11 +92,9 @@ void printOrders(const std::vector<Order> &vectorOfOrders) {
     std::cout << std::endl;
 }
 
-void takeQuantityFromStack(std::stack<char> &s, const int quantity){
-    std::vector<char> tmpElements;
-    for(int i = 0; i <  quantity; i++){
-        tmpElements.push_back(s.top());
-        s.pop();
+void printTmpStack(const std::vector<char> &tmpStack) {
+    for (const char c: tmpStack) {
+        std::cout << c << std::endl;
     }
 }
 
@@ -88,8 +103,16 @@ int main() {
     std::stack<char> stack1;
     std::stack<char> stack2;
     std::stack<char> stack3;
+    std::stack<char> stack4;
+    std::stack<char> stack5;
+    std::stack<char> stack6;
+    std::stack<char> stack7;
+    std::stack<char> stack8;
+    std::stack<char> stack9;
+    std::vector<char> takenElements;
 
-    std::vector<std::stack<char>> vectorOfStacks{stack1, stack2, stack3};
+    std::vector<std::stack<char>> vectorOfStacks{stack1, stack2, stack3, stack4, stack5, stack6, stack7, stack8,
+                                                 stack9};
 
     std::vector<Order> vectorOfOrders;
 
@@ -97,5 +120,8 @@ int main() {
     initializeStacks(vectorOfStacks);
     printStacks(vectorOfStacks);
     printOrders(vectorOfOrders);
-    takeQuantityFromStack(stack2, 1);
+    moveStacks(vectorOfStacks, vectorOfOrders, takenElements);
+    printTmpStack(takenElements);
+    printStacks(vectorOfStacks);
+    printStackTops(vectorOfStacks);
 }
