8ca3e39ae795ab9c601e89c90605f70add2af769
Ezdies <303214@office.umcs.pl>
2022-12-06 12:45:50 +0100

    Day 6 part 1

--- Day 6/1.cpp
+++ Day 6/1.cpp
@@ -3,31 +3,33 @@
 #include <unordered_set>
 #include <algorithm>
 
-
 void getInput(std::string &dataStream) {
     std::ifstream ist(R"(F:\CLionProjects\AoC2022\Day 6\data.txt)");
     while (ist >> dataStream);
 }
 
-bool areFourCharTheSame() {
-    const int size = 4;
+int result(const std::string &dataStream){
+    const int requiredSize = 4;
+    std::unordered_set<char> uniqueChars;
 
+    for (int i = 0; i < dataStream.length(); i++) {
+        std::string fragment = dataStream.substr(i, requiredSize);
 
+        for (const char c: fragment) {
+            uniqueChars.insert(c);
+        }
+        if (uniqueChars.size() == requiredSize) {
+             return i + requiredSize;
+        }
+        uniqueChars.clear();
+    }
+    return 0;
 }
 
 int main() {
     std::string dataStream;
     getInput(dataStream);
-
-    auto it = dataStream.begin();
-    for (int i = 0; i < dataStream.length(); i++) {
-        std::string fragment = dataStream.substr(i, 4);
-        it = std::unique(fragment.begin(), fragment.end());
-        if(fragment.end() == it){
-            std::cout <<  i;
-            break;
-        }
-    }
+    std::cout << result(dataStream);
 }
 
 
